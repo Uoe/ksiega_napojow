34aa151764152f5fb26b151d37ee006d70a5c523
Kinga Nowak <kinganowak@interia.eu>
2013-02-02 13:09:02 +0100

    dodano

---
+++ library/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '94358ba745dd3e63ae874dbb6409b6e09c4aefaaa4be203c62a73e581d44a72e85c15d1175bae3d27876509deafc05d432c807d611f6cd0a063b5bb41a28f1f8'
